93a655dff6a653b1af179c636bd4184981e4951d
eelien <eelienplus@icloud,cin>
2025-11-23 12:18:51 +0100

    fuck you 6

--- src/main/java/me/eelien/eelienmod/datagen/ModRecipeProvider.java
+++ src/main/java/me/eelien/eelienmod/datagen/ModRecipeProvider.java
@@ -1,11 +1,72 @@
 package me.eelien.eelienmod.datagen;
 
+import me.eelien.eelienmod.EElienMod;
+import me.eelien.eelienmod.block.ModBlocks;
+import me.eelien.eelienmod.item.ModItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.*;
+import net.minecraft.world.item.crafting.*;
+import net.minecraft.world.level.ItemLike;
+import net.neoforged.neoforge.common.conditions.IConditionBuilder;
+import net.minecraft.world.item.Items; // <-- REQUIRED for vanilla items
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
-public class ModRecipeProvider {
-    public ModRecipeProvider(PackOutput packOutput, CompletableFuture<HolderLookup.Provider> lookupProvider) {
+public class ModRecipeProvider extends RecipeProvider implements IConditionBuilder {
+    public ModRecipeProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
+        super(output, registries);
+    }
+
+    @Override
+    protected void buildRecipes(RecipeOutput recipeOutput) {
+        List<ItemLike> TIN_SMELTABLES = List.of(
+                ModBlocks.TIN_ORE);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.TIN_BLOCK.get())
+                .pattern("TTT")
+                .pattern("TTT") // Corrected: Must be 3 chars wide like the other patterns
+                .pattern("TTT")
+                .define('T', ModItems.TIN.get())
+                .unlockedBy("has_TIN", has(ModItems.TIN)).save(recipeOutput);
+
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.TIN.get(), 9)
+                .requires(ModBlocks.TIN_BLOCK)
+                .unlockedBy("has_TIN_block", has(ModBlocks.TIN_BLOCK)).save(recipeOutput);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, ModItems.SCREWDRIVER.get()) // Using TOOLS category
+                .pattern("CT ")
+                .pattern("TS ")
+                .pattern("  S")
+                // --- DEFINITIONS ADDED HERE ---
+                .define('C', Items.COPPER_INGOT) // Copper Ingot (Vanilla)
+                .define('T', ModItems.TIN.get())    // Tin Ingot (Modded)
+                .define('S', Items.STICK)        // Stick (Vanilla)
+                // ------------------------------
+                .unlockedBy("has_TIN", has(ModItems.TIN)).save(recipeOutput); // Required to finish recipe
+
+        oreSmelting(recipeOutput, TIN_SMELTABLES, RecipeCategory.MISC, ModItems.TIN.get(), 0.25f, 200, "TIN");
+        oreBlasting(recipeOutput, TIN_SMELTABLES, RecipeCategory.MISC, ModItems.TIN.get(), 0.25f, 100, "TIN");
+    }
+
+    protected static void oreSmelting(RecipeOutput recipeOutput, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult,
+                                      float pExperience, int pCookingTIme, String pGroup) {
+        oreCooking(recipeOutput, RecipeSerializer.SMELTING_RECIPE, SmeltingRecipe::new, pIngredients, pCategory, pResult,
+                pExperience, pCookingTIme, pGroup, "_from_smelting");
+    }
+
+    protected static void oreBlasting(RecipeOutput recipeOutput, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult,
+                                      float pExperience, int pCookingTime, String pGroup) {
+        oreCooking(recipeOutput, RecipeSerializer.BLASTING_RECIPE, BlastingRecipe::new, pIngredients, pCategory, pResult,
+                pExperience, pCookingTime, pGroup, "_from_blasting");
+    }
+
+    protected static <T extends AbstractCookingRecipe> void oreCooking(RecipeOutput recipeOutput, RecipeSerializer<T> pCookingSerializer, AbstractCookingRecipe.Factory<T> factory,
+                                                                       List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult, float pExperience, int pCookingTime, String pGroup, String pRecipeName) {
+        for(ItemLike itemlike : pIngredients) {
+            SimpleCookingRecipeBuilder.generic(Ingredient.of(itemlike), pCategory, pResult, pExperience, pCookingTime, pCookingSerializer, factory).group(pGroup).unlockedBy(getHasName(itemlike), has(itemlike))
+                    .save(recipeOutput, EElienMod.MODID + ":" + getItemName(pResult) + pRecipeName + "_" + getItemName(itemlike));
+        }
     }
 }
